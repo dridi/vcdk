1310f1aea2c8adda9657b93c4799c75e4a1857ff
Dridi Boukelmoune <dridi.boukelmoune@gmail.com>
2017-10-09 23:37:17 +0200

    GC dead code

--- plugins/autotools--codegen.sh
+++ plugins/autotools--codegen.sh
@@ -14,10 +14,6 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-needs_libtool() {
-	test -n "$vmod"
-}
-
 ###########
 # bootstrap
 ###########
